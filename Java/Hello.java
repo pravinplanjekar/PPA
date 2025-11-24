ca1cb4525cec02c7f280662e23e3f20accaf1360
pravinplanjekar <pravin.p.lanjekar@gmail.com>
2025-11-24 23:44:51 +0530

    Add files via upload

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,8 +1,10 @@
-
-class Demo
+class Hello
 {
     public static void main(String A[])
-    {   
+    {
         System.out.println("Jay Ganesh...");
     }
 }
+
+// javac Hello.java
+// java Hello       java Hello.java
